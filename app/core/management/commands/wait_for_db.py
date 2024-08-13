e3d903e5a7916469bf0e093ce3196217a57aac14
Luis Correa <luis_correa.tekton@skyairline.com>
2024-08-13 13:01:46 -0500

    update django test connections

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,29 +1,27 @@
-"""DJANGO COMMAND TO WAIT FOR DATABASE TO BE AVAILABLE
+"""
+Django command to wait for the database to be available.
 """
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
-
+from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to pause execution until database is available
-    """
-    
+    """Django command to wait for database."""
+
     def handle(self, *args, **options):
-        """Entru point for command
-        """
+        """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
-        db_up = False
-        while db_up is False:
+        db_conn = None
+        while not db_conn:
             try:
-                self.check(database=['default'])
-                db_up = True
-            except (Psycopg2OpError, OperationalError):
+                db_conn = connections['default']
+            except (OperationalError, Psycopg2OpError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)        
+                time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
